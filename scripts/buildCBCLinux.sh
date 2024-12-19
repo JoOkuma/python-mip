524b7a2b4fddc6f3a490f36612dc445281c026ec
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-12-18 16:41:15 -0800

    added linux build

--- scripts/buildCBCLinux.sh
+++ scripts/buildCBCLinux.sh
@@ -2,6 +2,9 @@ export CFLAGS="-Ofast -fPIC -flto -DNDEBUG -fprefetch-loop-arrays -I/opt/gcc/inc
 export FFLAGS="-Ofast -fPIC -flto -DNDEBUG -I/opt/gcc/include/"
 export CXXFLAGS="-Ofast -fPIC -flto -fprefetch-loop-arrays -DNDEBUG -I/opt/gcc/include/"
 export LDFLAGS="-Ofast -fPIC -L/opt/gcc/lib -flto -static-libgcc -static-libstdc++ -static-libgfortran"
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+
+"${CXX:=g++}"
 
 dir=`pwd`
 mkdir -p ~/prog
@@ -10,63 +13,67 @@ IDIR=`pwd`
 export PKG_CONFIG_PATH=${IDIR}/lib/pkgconfig/:${PKG_CONFIG_PATH}
 
 #cd $dir/ThirdParty-Metis
-#./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+#./configure --prefix=$IDIR --enable-static --disable-shared
 #make -j 6
 #make -j 6 install
 
 cd $dir/ThirdParty-Blas
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 cd $dir/ThirdParty-Lapack
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 #cd $dir/ThirdParty-Mumps
-#./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+#./configure --prefix=$IDIR --enable-static --disable-shared
 #make -j 6
 #make -j 6 install
 
 cd $dir/ThirdParty-Glpk
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 
 cd $dir/CoinUtils
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 cd $dir/Osi
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 cd $dir/Clp
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 cd $dir/Cgl
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared
 make -j 6
 make -j 6 install
 
 
 cd $dir/Cbc
-./configure --prefix=$IDIR --enable-cbc-parallel --enable-static --disable-shared --enable-gnu-packages
+./configure --prefix=$IDIR --enable-static --disable-shared --with-readline=$CONDA_PREFIX
 make -j 6
 make -j 6 install
 
 cd $dir
 
-g++ -shared -Ofast -fPIC -o ../mip/libraries/cbc-c-linux-x86-64.so \
--I${IDIR}/include/coin-or/ \
+patch -u ${IDIR}/include/coin-or/CoinMpsIO.hpp -i coinutils-glpk.patch
+
+$CXX -shared -Ofast -fPIC -o ../mip/libraries/cbc-c-linux-x86-64.so \
+ -I${IDIR}/include/coin-or/ \
  -DCBC_THREAD \
  ./Cbc/src/Cbc_C_Interface.cpp \
  -L/opt/gcc/lib64/ -L${IDIR}/lib/ \
- -lCbcSolver -lCbc -lpthread -lrt -lCgl -lOsiClp -lClpSolver -lClp -lOsi -lCoinUtils \
+ -lCbc -lCgl -lClp -lCoinUtils -lOsi -lOsiCbc -lOsiClp -lOsiGlpk \
+ -lcoinblas -lcoinglpk -lcoinlapack \
+ -lreadline -lbz2 -lz \
  -lcoinlapack -lcoinblas -lgfortran -lquadmath -lm -static-libgcc -static-libstdc++ -static-libgfortran -lcoinglpk
